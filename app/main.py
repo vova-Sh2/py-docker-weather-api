9f836549a2efeeb1120111517c4ca3099e81feae
Vova <vovakucin082@gmail.com>
2026-05-12 13:20:23 +0200

    FIX

--- app/main.py
+++ app/main.py
@@ -13,8 +13,10 @@ def get_weather() -> None:
     response = requests.get(URL + f"key={API_KEY}&q={FILTERING}&aqi={AQI}")
     location = response.json()["location"]
     current = response.json()["current"]
+
     print(f"{location["name"]}/{location["country"]} {location["localtime"]} "
-          f"Weather: {current["temp_c"]} Celsius, {current["condition"]["text"]}")
+          f"Weather: {current["temp_c"]} Celsius, "
+          f"{current["condition"]["text"]}")
 
 
 if __name__ == "__main__":
