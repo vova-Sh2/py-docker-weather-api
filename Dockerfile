fce76dcd19bfb66812da057a17fd8466ab5f6504
Vova <vovakucin082@gmail.com>
2026-05-12 13:17:43 +0200

    Code Write

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.13.13-alpine3.23
+LABEL maintainer="vovakucin082@gmail.com"
+
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+import os
+
+import requests
+
+
+URL = "http://api.weatherapi.com/v1/current.json?"
+AQI = "no"
+API_KEY = os.getenv("API_KEY")
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(URL + f"key={API_KEY}&q={FILTERING}&aqi={AQI}")
+    location = response.json()["location"]
+    current = response.json()["current"]
+    print(f"{location["name"]}/{location["country"]} {location["localtime"]} "
+          f"Weather: {current["temp_c"]} Celsius, {current["condition"]["text"]}")
 
 
 if __name__ == "__main__":
